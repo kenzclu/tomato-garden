e461cd3e666067bfc0b8f14b20a30a28cba21a45
Haley Gu <haley.gu@student.unsw.edu.au>
2021-07-18 02:22:11 +1000

    cute landing page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import avatar from '../assets/avatar1.png'
+
+import './Landing.css'
 
 function Landing() {
   return (
-    <div className='landing-container'>
-      <h1>Landing Page</h1>
+    <div className='background'>
+      <div className='landing-paper'>
+        <img src={avatar} alt='avatar 1' height='300rem' width='auto' />
+        <h1>What do you want to achieve today?</h1>
+        <input className='goal-input' type='text' />
+      </div>
     </div>
   );
 }

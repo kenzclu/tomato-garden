4be7f9d3d6c4acadacf56209610e1767cb8fdb8b
Kevin Wu <kevinwu1229@hotmail.com>
2021-07-18 03:22:02 +1000

    finished fixed sidebar

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -9,7 +9,7 @@ function Sidebar() {
   return (
     <div className='sidebar'>
       <div className='avatar'>
-        <img src={avatar} alt='avatar 1' height='300rem' width='auto' />
+        <img src={avatar} alt='avatar 1' height='75%' width='auto' />
         <h1>Anonymous Hippo</h1>
       </div>
       {/* <h2>Todo</h2>
@@ -17,7 +17,30 @@ function Sidebar() {
         <img className='checkbox' src={checkbox} alt='checkbox' height='15px' width='15px' />
         <p>complete my math homework</p>
       </div> */}
-      <div>
+      <div className='todo-title'>Todo</div>
+        <div className='todo-container'> 
+          <label className='container'>
+            <input type='checkbox'></input>
+            <span className='checkmark'></span>
+            Complete my math homework
+          </label>
+          <label className='container'>
+            <input type='checkbox'></input>
+            <span className='checkmark'></span>
+            Complete my compsci homework
+          </label>
+          <label className='container'>
+            <input type='checkbox'></input>
+            <span className='checkmark'></span>
+            Smacking Kenneth up
+          </label>
+          <label className='container'>
+            <input type='checkbox'></input>
+            <span className='checkmark'></span>
+            Demoting back to b2
+          </label>
+        </div>
+      <div className='chat'>
         <h2>Chat</h2>
         <Chat />
       </div>
